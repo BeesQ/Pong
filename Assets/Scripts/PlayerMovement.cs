6979ca9f4acc9fb4a5fbd086273121edfebf6b72
BeesQ <jakublotysz@gmail.com>
2023-04-26 19:40:52 +0200

    Fixing Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,9 +15,18 @@ public class PlayerMovement : MonoBehaviour
     public void HandleMovement()
     {
         var y = Input.GetAxisRaw("Vertical") * speedForce;
-        myRb.velocity = new Vector2(0, y) * Time.fixedDeltaTime;
+        var newVelocity = new Vector2(0, y) * Time.fixedDeltaTime;
 
         MyBounds cameraBounds = CameraManager.GetCameraBounds();
-        transform.position = new Vector2(transform.position.x, Mathf.Clamp(transform.position.y, cameraBounds.minY, cameraBounds.maxY));
+        // transform.position = new Vector2(transform.position.x, Mathf.Clamp(transform.position.y, cameraBounds.minY, cameraBounds.maxY));
+        if (transform.position.y > cameraBounds.maxY && newVelocity.y > 0)
+        {
+            newVelocity = Vector2.zero;
+        }
+        else if (transform.position.y < cameraBounds.minY && newVelocity.y < 0)
+        {
+            newVelocity = Vector2.zero;
+        }
+        myRb.velocity = newVelocity;
     }
 }
